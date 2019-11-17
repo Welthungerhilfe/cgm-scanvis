5dd7df1fbcba125c8ae11eab4331d539998d309b
Amie Everett <amie.everett@gmail.com>
2019-11-16 19:09:21 -0500

    commit work for the day

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,31 @@ import {Card, CardText, CardBody, CardTitle, CardImg, CardSubtitle, Button} from
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
-  
+
+    // Once user is authenticated and data files are loaded, look for the JSON files in each child's folder and generate state object for that. The key is the QR code, and the value is an object which has key-value pairs for each of the JSON values we want, for example:
+
+    // MH_WHH_0123: {
+    //   deeplinkPath: 'dir/filename',
+    //   consentExists: false,
+    //   consentLegible: false,
+    //   isTestData: false,
+    //   isValidated: false,
+    //   pose: {
+    //     standing: false,
+    //     lying: true,
+    //     notAvailable: false
+    //   },
+    //   angle: {
+    //     front: true,
+    //     back: true,
+    //     '360': false,
+    //   }
+    // }
+    
+    // If any of the folders are missing the JSON file, call GenerateJson with that folder and the data contained in it. We are returned the object from that and then that object can be saved in state.
+
+    // Later we will try to save the actual file into JSON format and push to back end (alternatively save on Data Scientist's computer and upload at later time)
+
     handleChange(event) {
       this.setState({ name: event.target.value });
     }
